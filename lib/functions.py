1b627254a820490fd7c1a4aa6c5b5b674f116cc3
merialea <merialea5@gmail.com>
2025-11-12 16:55:03 +0300

    complete lab

--- lib/functions.py
+++ lib/functions.py
@@ -10,7 +10,7 @@ def greet_with_default(name="programmer"):
     print(f"Hello, {name}!")
 
 def add(num1, num2):
-    return num1 + num2;
+    return num1 + num2
 
 def halve(number):
-    return number / 2;
+    return number / 2
